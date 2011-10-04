9ac02fc91c33d8eb6b72b316a154a43e8a0dcb8a
Emily Lucas <eclucas@calpoly.edu>
2011-10-04 01:01:42 -0700

    Added Instance Variables

--- ListExtravaganza.java
+++ ListExtravaganza.java
@@ -1,2 +1,9 @@
+import java.util.ArrayList;
+
 public class ListExtravaganza
-{}
+{
+   ArrayList<Integer> listInteger;
+   ArrayList<Double> listDouble;
+   ArrayList<Boolean> listBoolean;
+   ArrayList<String> listString;   
+}
