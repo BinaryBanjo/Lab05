ae443e558ef6d8e643cde4e9c469ad1d875b6be4
Emily Lucas <eclucas@calpoly.edu>
2011-10-04 02:28:58 -0700

    Class: Added Scanner.

--- ListExtravaganza.java
+++ ListExtravaganza.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ListExtravaganza
 {
+   private Scanner in;
    private ArrayList<Integer> listInteger;
    private ArrayList<Double> listDouble;
    private ArrayList<Boolean> listBoolean;
@@ -9,13 +11,37 @@ public class ListExtravaganza
    
    public ListExtravaganza()
    {
-
+   
+      in = new Scanner(System.in);
       listInteger = new ArrayList<Integer>();
       listDouble = new ArrayList<Double>();
       listBoolean = new ArrayList<Boolean>();
       listString = new ArrayList<String>();   
       
-   }
+     /* boolean cont = true;
+      String placeHolder = "";
+      
+      while ((cont))
+      {
+         System.out.println("\nPlease enter an integer, "
+                   + "boolean, double, or String:  ");
+         
+         if(in.hasNextInt())
+         {
+            add(in.nextInt());
+         }
+         else
+         {
+            placeHolder = in.next();
+            
+            if (placeHolder.equals("quit"))
+            {
+               cont = false;
+            }
+         }
+      }//end while ((cont)) 
+     */
+   }//end constructor
    
    public void add (Integer inputInt)
    {
@@ -32,4 +58,4 @@ public class ListExtravaganza
       return listInteger.size();
    }
    
-}
+}//end ListExtravaganza
