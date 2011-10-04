7243240993bfc178c2f51bd3009054705594c327
Emily Lucas <eclucas@calpoly.edu>
2011-10-04 01:15:25 -0700

    Added default constructor

--- ListExtravaganza.java
+++ ListExtravaganza.java
@@ -6,4 +6,14 @@ public class ListExtravaganza
    private ArrayList<Double> listDouble;
    private ArrayList<Boolean> listBoolean;
    private ArrayList<String> listString;   
+   
+   public ListExtravaganza()
+   {
+
+      listInteger = new ArrayList<Integer>();
+      listDouble = new ArrayList<Double>();
+      listBoolean = new ArrayList<Boolean>();
+      listString = new ArrayList<String>();   
+      
+   }
 }
